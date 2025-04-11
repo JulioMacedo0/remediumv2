e10f8b852382a85d1dcdc1b7c9f11b4df36cc525
Julio Macedo <juliocesar1076@gmail.com>
2025-04-11 09:51:44 -0300

    feat: add protected route logic

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import {AppStack} from './AppStack';
 import {AuthStack} from './AuthStack';
-
-const authenticated = false;
+import {useAuthStore} from '../stores/auth/authStore';
+import {SplashScreen} from '../components/SplashScreen/SplashScreen';
 
 export function Router() {
+  const token = useAuthStore(s => s.token);
+  const restoreSession = useAuthStore(s => s.restoreSession);
+  const [restoring, setRestoring] = useState(true);
+
+  const restoreSessionFn = async () => {
+    await restoreSession();
+    setRestoring(false);
+  };
+
+  useEffect(() => {
+    restoreSessionFn();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <NavigationContainer>
-      {authenticated ? <AppStack /> : <AuthStack />}
+      {restoring ? <SplashScreen /> : token ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
   );
 }
