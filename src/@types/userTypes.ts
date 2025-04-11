d28680be307230bbad6dd4289a76397627fe21db
Julio Macedo <juliocesar1076@gmail.com>
2025-04-11 09:52:12 -0300

    feat: create user type

---
+++ src/@types/userTypes.ts
@@ -0,0 +1,10 @@
+export type UserType = {
+  id: string;
+  username: string;
+  email: string;
+  languageTag: String;
+  timeZone: String;
+  createdAt: Date;
+  updatedAt: Date;
+  expo_token: string;
+};
