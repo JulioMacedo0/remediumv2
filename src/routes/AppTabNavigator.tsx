d79b69e9576e51ddfab2fc67643208c86ba4e173
Julio Macedo <juliocesar1076@gmail.com>
2025-04-15 10:13:46 -0300

    feat: init one signal on home page

--- src/routes/AppTabNavigator.tsx
+++ src/routes/AppTabNavigator.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {
   BottomTabBarProps,
@@ -8,6 +8,8 @@ import {HomeScreen} from '../screens/app/HomeScreen/HomeScreen';
 import {CreateAlertScreen} from '../screens/app/CreateAlertScreen/CreateAlertScreen';
 import {SettingsScreen} from '../screens/app/SettingsScreen/SettingsScreen';
 import {AppTabBar} from './ AppTabBar';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
+import {env} from '../env';
 
 export type AppTabBottomTabParamList = {
   HomeScreen: undefined;
@@ -18,6 +20,16 @@ export type AppTabBottomTabParamList = {
 const Tab = createBottomTabNavigator<AppTabBottomTabParamList>();
 
 export function AppTabNavigator() {
+  useEffect(() => {
+    // Enable verbose logging for debugging (remove in production)
+    OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+    // Initialize with your OneSignal App ID
+    OneSignal.initialize(env.ONE_SIGNAL_APP_ID);
+    // Use this method to prompt for push notifications.
+    // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
+    OneSignal.Notifications.requestPermission(false);
+  }, []);
+
   function renderTabBar(props: BottomTabBarProps) {
     return <AppTabBar {...props} />;
   }
