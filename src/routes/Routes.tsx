142828ac4472a1a3ca821913415b1a89ec7d8a6c
Julio Macedo <juliocesar1076@gmail.com>
2025-05-08 08:17:33 -0300

    feat: init one signal

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -8,6 +8,8 @@ import {SplashScreen} from '../components/SplashScreen/SplashScreen';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {StyleSheet} from 'react-native';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
+import {env} from '../env';
 
 export function Router() {
   const token = useAuthStore(s => s.token);
@@ -18,6 +20,16 @@ export function Router() {
     await restoreSession();
     setRestoring(false);
   };
+
+  useEffect(() => {
+    // Enable verbose logging for debugging (remove in production)
+    OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+    // Initialize with your OneSignal App ID
+    OneSignal.initialize(env.ONE_SIGNAL_APP_ID);
+    // Use this method to prompt for push notifications.
+    // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
+    OneSignal.Notifications.requestPermission(false);
+  }, []);
 
   useEffect(() => {
     restoreSessionFn();
